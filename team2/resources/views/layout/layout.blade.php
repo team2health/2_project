2a578c47b5fcc050d2d3c70b8c350531b91ac4ca
mk1206 <alsrod1400@naver.com>
2023-12-11 14:28:51 +0900

    231211_css

--- team2/resources/views/layout/layout.blade.php
+++ team2/resources/views/layout/layout.blade.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="stylesheet" href="/css/mypage.css">
+	<link rel="stylesheet" href="/css/board.css">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>@yield('title', 'main')</title>
 </head>
