af4eaa8a2e62520763df0465cec653d0230af337
mk1206 <alsrod1400@naver.com>
2023-12-11 17:25:42 +0900

    231211_header 반응형

--- team2/resources/views/layout/header.blade.php
+++ team2/resources/views/layout/header.blade.php
@@ -1,14 +1,17 @@
 <header>
 	<div class="container">
-		<div>2023.12.11</div>
-		<div>월요일</div>
+		<div class="div-margin">
+			<div>2023.12.11</div>
+			<div>월요일</div>
+		</div>
+
 		<div class="display-flex">
-			<div class="div-display-lnlineBlock">
+			<div class="div-display-lnlineBlock display-none">
 				<button type="submit">
 					<img src="../img/d.jpg" alt="" class="btn-img">
 				</button>
 			</div>
-			<div class="div-display-lnlineBlock display-flex-center">
+			<div class="display-flex-center">
 				<div class="div-display-lnlineBlock"><img src="../img/f-img.png" alt="" class="btn-img"></div>
 				<div class="div-display-lnlineBlock">
 					<div><span class="font-weight font-color">동글이</span>님 안녕하세요</div>
@@ -18,6 +21,12 @@
 					</a>
 				</div>
 			</div>
+		</div>
+
+		<div class="container-category display-flex-around div-padding">
+			<a href="/" class="div-display-lnlineBlock">증상 검색</a>
+			<a href="/" class="div-display-lnlineBlock">커뮤니티</a>
+			<a href="/" class="div-display-lnlineBlock">타임라인</a>
 		</div>
 	</div>
 </header>
