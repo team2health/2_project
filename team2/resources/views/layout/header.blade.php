7c46662901d5734de5f01ee0e484777d20c6097d
mk1206 <alsrod1400@naver.com>
2023-12-11 15:59:57 +0900

    231211_header 초안

--- team2/resources/views/layout/header.blade.php
+++ team2/resources/views/layout/header.blade.php
@@ -1,3 +1,23 @@
 <header>
-	<div>헤더</div>
+	<div class="container">
+		<div>2023.12.11</div>
+		<div>월요일</div>
+		<div class="display-flex">
+			<div class="div-display-lnlineBlock">
+				<button type="submit">
+					<img src="../img/d.jpg" alt="" class="btn-img">
+				</button>
+			</div>
+			<div class="div-display-lnlineBlock display-flex-center">
+				<div class="div-display-lnlineBlock"><img src="../img/f-img.png" alt="" class="btn-img"></div>
+				<div class="div-display-lnlineBlock">
+					<div><span class="font-weight font-color">동글이</span>님 안녕하세요</div>
+					<a href="/" class="display-flex font-small">
+						<span>welcome</span>
+						<span class="font-color">마이페이지 이동</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </header>
