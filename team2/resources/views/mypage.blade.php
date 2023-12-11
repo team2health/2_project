f7a2892ad6a0f069d05cf08e0b69d1b439f72e9e
kanghwiya <fiminmy@gmail.com>
2023-12-11 18:11:10 +0900

    231211 마이페이지 달력

---
+++ team2/resources/views/mypage.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layout.layout');
+
+@section('title','mypage');
+    
+
+@section('main')
+
+<div class="mypage-btn-layout">
+    <div class="mypage-btn-line">
+        <div class="mypage-btn">정보수정</div>
+        <div class="mypage-btn">타임라인</div>
+    </div>
+    <div class="mypage-btn">나의 게시물</div>
+</div>
+
+<div class="mypage-title">내가 찜한 관심 태그</div>
+
+@endsection
