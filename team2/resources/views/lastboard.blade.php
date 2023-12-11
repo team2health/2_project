10811d3886fe2e7682dba31ea74a93c74057d0b9
rokgt <knight_gt@naver.com>
2023-12-11 17:01:16 +0900

    231211 라스트보드업데이트

---
+++ team2/resources/views/lastboard.blade.php
@@ -0,0 +1,103 @@
+@extends('layout.layout')
+
+@section('title','Lastboard')
+
+@section('main')
+
+<main class="last_main">
+    <div class="last_headline">
+        <h2>자유게시판</h2>
+        <button type="submit">
+            정렬
+        </button>
+    </div>
+    <div class="last_container">
+        <div class="last_user">
+            <div>
+                닉네임
+            </div>
+            <div>
+                작성일자
+            </div>
+        </div> 
+        <div>
+            제목
+        </div> 
+        <div class="last_content">
+            내용
+        </div>  
+    </div>
+    
+    <div class="last_container">
+        <div class="last_user">
+            <div>
+                닉네임
+            </div>
+            <div>
+                작성일자
+            </div>
+        </div> 
+        <div>
+            제목
+        </div> 
+        <div class="last_content">
+            내용
+        </div>  
+    </div>
+    <div class="last_container">
+        <div class="last_user">
+            <div>
+                닉네임
+            </div>
+            <div>
+                작성일자
+            </div>
+        </div> 
+        <div>
+            제목
+        </div> 
+        <div class="last_content">
+            내용
+        </div>  
+    </div>
+    <div class="last_container">
+        <div class="last_user">
+            <div>
+                닉네임
+            </div>
+            <div>
+                작성일자
+            </div>
+        </div> 
+        <div>
+            제목
+        </div> 
+        <div class="last_content">
+            내용
+        </div>  
+    </div>
+    <div class="last_headline">
+        <h2>질문게시판</h2>        
+    </div>
+    <div class="last_container">
+        <div class="last_user">
+            <div>
+                닉네임
+            </div>
+            <div>
+                작성일자
+            </div>
+        </div> 
+        <div>
+            제목
+        </div> 
+        <div class="last_content">
+            내용
+        </div>  
+    </div>
+
+
+
+</main>
+
+@endsection
