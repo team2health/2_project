cb1ae144c0bcf7fea6813cfa9180bf52eabfc7e2
mk1206 <alsrod1400@naver.com>
2023-12-11 13:10:10 +0900

    231211_controller, model, migration 생성

---
+++ team2/app/Models/Part_symptom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Part_symptom extends Model
+{
+    use HasFactory;
+}
