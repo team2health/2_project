49477667bab997b3deb28163bbc83b54831b10a8
kanghwiya <fiminmy@gmail.com>
2023-12-13 21:52:13 +0900

    231213 마이페이지 css 수정

--- team2/resources/views/mypage.blade.php
+++ team2/resources/views/mypage.blade.php
@@ -5,11 +5,11 @@
 
 @section('main')
 
-
-
 <div class="mypage-grid" id="mypageGrid">
     <div class="mypage-content" id="mypageContent">
 
+            <div class="mypage-board-show-btn">내가 쓴 게시글</div>
+            <div class="mypage-comment-show-btn">내가 쓴 댓글</div>
         {{-- 게시글 창 --}}
             <div class="mypage-date-today">
                 <span class="mypage-board-date">2023-12-12</span>
@@ -33,30 +33,37 @@
                 </div>
             </div>
 
-        <div class="mypage-date-today">
-            <span class="mypage-board-date">2023-12-12</span>
-        </div>
-        <div class="mypage-boards-part">
-            <div class="mypage-boardbox">
-                <div class="mypage-bord-title">제목입니다.</div>
-                <div class="mypage-bord-detailbox">상세내용입니다.</div>
+            <div class="mypage-date-today">
+                <span class="mypage-board-date"></span>
             </div>
-            <div class="mypage-boardbox">
-                <div class="mypage-bord-title">제목입니다.</div>
-                <div class="mypage-bord-detailbox">상세내용입니다.</div>
+            <div class="mypage-boards-part">
+                <div class="mypage-boardbox">
+                    <span class="mypage-boardbox-date">2023-12-12</span>
+                    <div class="mypage-bord-title"> 댓글 view 제목입니다.</div>
+                    <div class="mypage-bord-detailbox">상세내용입니다.</div>
+                </div>
+                <div class="mypage-boardbox">
+                    <spa class="mypage-boardbox-date">2023-12-12</spa>
+                    <div class="mypage-bord-title">제목입니다.</div>
+                    <div class="mypage-bord-detailbox">상세내용입니다.</div>
+                </div>
+                <div class="mypage-boardbox">
+                    <span class="mypage-boardbox-date">2023-12-12</span>
+                    <div class="mypage-bord-title">제목입니다.</div>
+                    <div class="mypage-bord-detailbox">상세내용입니다.</div>
+                </div>
+                <div class="mypage-boardbox">
+                    <span class="mypage-boardbox-date">2023-12-12</span>
+                    <div class="mypage-bord-title">제목입니다.</div>
+                    <div class="mypage-bord-detailbox">상세내용입니다.</div>
+                </div>
             </div>
-            <div class="mypage-boardbox">
-                <div class="mypage-bord-title">제목입니다.</div>
-                <div class="mypage-bord-detailbox">상세내용입니다.</div>
-            </div>
-            <div class="mypage-boardbox">
-                <div class="mypage-bord-title">제목입니다.</div>
-                <div class="mypage-bord-detailbox">상세내용입니다.</div>
-            </div>
-        </div>
 
         <div class="mypage-btn-plus">더보기</div>
-
+        {{-- 게시글 insert로 넘어감 --}}
+        <a href="{{route('insert')}}">
+            <img src="/img/plusbtn.png" alt="" class="mypage-insert-btn">
+        </a>
     </div>
 
     {{-- 정보수정창 --}}
@@ -67,19 +74,27 @@
                 </div>
             </label>
             <input type="file" accept="image/*" style="display: none;" id="profilephoto">
+            <br>
             <label for="profilephoto" class="user-info-btn"> 사진 변경 </label>
             <div class="user-info-btn"> 삭제 </div>
-
+            <br>
             <label for="usermodifyname">닉네임 수정</label>
             <input type="text" id="usermodifyname">
+            <br>
             <label for="usermodifypassword">비밀번호 수정</label>
             <input type="password" id="usermodifypassword">
+            <br>
             <label for="usermodifypasswordchk">비밀번호 확인</label>
             <input type="password" id="usermodifypasswordchk">
-            <label for="usermodifyaddress">주소</label>
-            <input type="text" id="usermodifyaddress">
-            <label for="usermodifybirth">생년월일</label>
-            <input type="text" id="usermodifybirth">
+            <br>
+			<div class="adress-box">
+				<input type="text" id="sample4_postcode" placeholder="우편번호">
+				<input type="button" onclick="sample4_execDaumPostcode()" value="우편번호 찾기" class="user-info-btn"><br>
+				<span id="guide" style="color:#999;display:none"></span>
+				<input type="text" id="sample4_roadAddress" placeholder="도로명주소" name="user_adress_f">
+				<br>
+				<input type="text" id="sample4_detailAddress" placeholder="상세주소" name="user_adress_s">
+			</div>
             
         </div>
     </div>
@@ -110,9 +125,9 @@
             </div>
         </div>
     </div>
-    
 </div>
 
 
 <script src="/js/mypage.js"></script>
+<script src="/js/main.js"></script>
 @endsection
