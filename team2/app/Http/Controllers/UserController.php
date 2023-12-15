887ba81ab99029d00709391e8a9f905de69615b5
kanghwiya <fiminmy@gmail.com>
2023-12-15 10:05:30 +0900

    231215_유저컨트롤러 수정

--- team2/app/Http/Controllers/UserController.php
+++ team2/app/Http/Controllers/UserController.php
@@ -74,12 +74,16 @@ class UserController extends Controller
     // 마이페이지 이동 시 로그인 유무확인 및 게시글 불러오기
     public function mypageget(Request $request) {
 
+        // 사용자 ID 가져오기
+        $result2 = session('user_id');
+
         $result = User::where('user_id', $request->user_id)->first();
-        var_dump($result);
+
+        var_dump($result2);
         if(Auth::check()) {
             $boardresult = Board::where('u_id', $result)->get();
-            // var_dump($boardresult);
-            // exit;
+            var_dump($boardresult);
+            exit;
             var_dump($boardresult);
             exit;
             return view('mypage')->with('data', $boardresult);
