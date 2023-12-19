4397d84fb2fa4ecc5c73988136a48595411f1440
kanghwiya <fiminmy@gmail.com>
2023-12-19 09:53:06 +0900

    231219_마이페이지css확인

--- team2/database/seeders/HashtagSeeder.php
+++ team2/database/seeders/HashtagSeeder.php
@@ -16,21 +16,22 @@ class HashtagSeeder extends Seeder
     public function run()
     {
         DB::table('hashtags')->insert([
-            ['hashtag_id' => '1', 'hashtag_name' => '#발열']
-			,['hashtag_id' => '2', 'hashtag_name' => '#편두통']
-			,['hashtag_id' => '3', 'hashtag_name' => '#꾀병']
-			,['hashtag_id' => '4', 'hashtag_name' => '#자유']
-			,['hashtag_id' => '5', 'hashtag_name' => '#친목']
-			,['hashtag_id' => '6', 'hashtag_name' => '#운동']
-			,['hashtag_id' => '7', 'hashtag_name' => '#복통']
-			,['hashtag_id' => '8', 'hashtag_name' => '#안구건조증']
-			,['hashtag_id' => '9', 'hashtag_name' => '#체함']
-			,['hashtag_id' => '10', 'hashtag_name' => '#해']
-			,['hashtag_id' => '11', 'hashtag_name' => '#달']
-			,['hashtag_id' => '12', 'hashtag_name' => '#별']
-			,['hashtag_id' => '13', 'hashtag_name' => '#염좌']
-			,['hashtag_id' => '14', 'hashtag_name' => '#손목터널증후군']
-			,['hashtag_id' => '15', 'hashtag_name' => '#어깨결림']
+            ['hashtag_id' => '16', 'hashtag_name' => '#영원']
+			,['hashtag_id' => '17', 'hashtag_name' => '#서시']
+			,['hashtag_id' => '18', 'hashtag_name' => '#별헤는밤']
+			,['hashtag_id' => '19', 'hashtag_name' => '#윤동주']
+			,['hashtag_id' => '20', 'hashtag_name' => '#별하나']
+			,['hashtag_id' => '21', 'hashtag_name' => '#추억']
+			,['hashtag_id' => '22', 'hashtag_name' => '#사랑']
+			,['hashtag_id' => '23', 'hashtag_name' => '#동경']
+			,['hashtag_id' => '24', 'hashtag_name' => '#냠냠']
+			,['hashtag_id' => '25', 'hashtag_name' => '#시']
+			,['hashtag_id' => '26', 'hashtag_name' => '#쓸쓸함']
+			,['hashtag_id' => '27', 'hashtag_name' => '#우주']
+			,['hashtag_id' => '28', 'hashtag_name' => '#북간도']
+			,['hashtag_id' => '29', 'hashtag_name' => '#박민경']
+			,['hashtag_id' => '30', 'hashtag_name' => '#강휘야']
+			,['hashtag_id' => '31', 'hashtag_name' => '#유현호']
         ]);
     }
 }
