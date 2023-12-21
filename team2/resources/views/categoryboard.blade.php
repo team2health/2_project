fc3284ed96e7e58b823129b7c764c27b34fcdc85
rokgt <knight_gt@naver.com>
2023-12-21 10:06:53 +0900

    2023 12 21 오전 10시 유저이미지수정

--- team2/resources/views/categoryboard.blade.php
+++ team2/resources/views/categoryboard.blade.php
@@ -26,8 +26,10 @@
     
     @foreach ($data[0] as $item)
     <div class="last_container">
-        <div class="last_user">                
-             <img class="community_icon" src="{{ asset('public/user_img/' . optional($item->user)->user_img) }}" class="board_nic_img" alt="User Image">            
+        <div class="last_user">  
+                        
+             <img class="community_icon" src="{{ asset('user_img/' . optional($item->user)->user_img) }}" class="board_nic_img" alt="User Image">            
+            
             <div class="board_nic_text">
                 <div>
                 {{ optional($item->user)->user_name }}
