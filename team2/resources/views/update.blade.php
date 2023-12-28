8334bb54507b4a83ed0765825e938bb774c36a1b
rokgt <knight_gt@naver.com>
2023-12-28 16:47:19 +0900

    풀용

--- team2/resources/views/update.blade.php
+++ team2/resources/views/update.blade.php
@@ -10,25 +10,8 @@
 			@csrf
 			@method('PUT')            
 			<div class="insert_container">           
-				<!-- <div class="insert_img">
-					{{-- @foreach($result->images as $key => $image) --}}
-						<div class="detail_board_content">
-							{{-- <img src="/board_img/{{ $image->img_address }}" alt="Board Image" id="preview{{ $key }}"> --}}
-							{{-- <label for="file{{ $key }}"> --}}
-								{{-- <button type="button" onclick="openFile('file{{ $key }}')">파일변경</button> --}}
-							</label>
-							{{-- <input type="file" name="board_img[]" id="file{{ $key }}" style="display:none;" onchange="previewImage('file{{ $key }}', 'preview{{ $key }}')" accept="image/*"> --}}
-						</div>
-					{{-- @endforeach --}}
-				</div>	
-				<div class="detail_board_content">
-        
-        <label for="file_new">
-			{{-- <img id="preview_new" src="{{ asset('img/plus.png') }}" alt=""> --}}
-        </label>
-        <input type="file" name="board_img[new]" id="file_new" style="display:none;" onchange="previewImage('file_new', 'preview_new')" accept="image/*">
-    </div> -->
-	<!-- <div class="insert_img">
+				
+	<div class="insert_img">
     @for ($i = 0; $i < 3; $i++)
         @php
             $imgId = 'preview' . $i;
@@ -43,24 +26,9 @@
         </label>
         <input type="file" name="board_img[]" id="{{ $fileId }}" style="display:none;" onchange="previewImage('{{ $fileId }}', '{{ $imgId }}')" accept="image/*">
     @endfor
-</div> -->
- 	
-<div class="insert_img">
-    <label for="file0">
-        <img id="preview0" src="{{ isset($result->images[0]) ? asset('board_img/' . $result->images[0]->img_address) : asset('img/plus.png') }}" alt="{{ isset($result->images[0]) ? 'Image 1' : 'Placeholder Image' }}">
-    </label>
-    <input type="file" name="board_img[]" id="file0" style="display:none;" onchange="previewImage('file0', 'preview0')" accept="image/*" >
-
-    <label for="file1">
-        <img id="preview1" src="{{ isset($result->images[1]) ? asset('board_img/' . $result->images[1]->img_address) : asset('img/plus.png') }}" alt="{{ isset($result->images[1]) ? 'Image 2' : 'Placeholder Image' }}">
-    </label>
-    <input type="file" name="board_img[]" id="file1" style="display:none;" onchange="previewImage('file1', 'preview1')" accept="image/*" >
-
-    <label for="file2">
-        <img id="preview2" src="{{ isset($result->images[2]) ? asset('board_img/' . $result->images[2]->img_address) : asset('img/plus.png') }}" alt="{{ isset($result->images[2]) ? 'Image 3' : 'Placeholder Image' }}">
-    </label>
-    <input type="file" name="board_img[]" id="file2" style="display:none;" onchange="previewImage('file2', 'preview2')" accept="image/*" >
 </div>
+ 	
+
 				<div class="insert_select_container">
 					<select name="board" id="board" class="insert_select">
 					
@@ -104,21 +72,7 @@
 			</div>
 			
 	
-			<!-- <div class="insert_hashtag_container">
-				<label for="hashtag" class="label_hashtag">#해시태그</label>
-				<input class="insert_hashtag" id="hashtagContainer" value="{{ implode(',', $result->hashtags->pluck('hashtag_name')->toArray()) }}" readonly>
-				<button type="button" id="toggleHashtagsBtn" onclick="toggleHashtags()">해시태그 펼치기/접기</button>
-				<div id="hiddenHashtags" style="display: none;">
-					@foreach ($allHashtags as $hashtag)
-						<span class='tag' data-tag="{{ $hashtag->hashtag_name }}">							
-							{{ $hashtag->hashtag_name }}
-						</span>
-					@endforeach
-				</div>
-				Input for selected hashtags
-				<input type="hidden" id="selectedHashtagsInput" name="hashtag" />
-			</div> -->
-							
+				
 			
 			
 	</div>
