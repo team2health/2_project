0b3edd57dc8c4da6d0aff0a22e2e0995b7a57080
mk1206 <alsrod1400@naver.com>
2024-01-04 12:45:31 +0900

    240104_footer

--- team2/resources/views/community.blade.php
+++ team2/resources/views/community.blade.php
@@ -3,128 +3,21 @@
 @section('title','Community')
 
 @section('main')
-<main class="">
-    <div class="slider-container">
-        <a href="#top" class="community_a"><img class="community_icon" src="../img/top.png" alt=""></a>
-        <a href="{{route('board.create')}}" class="community_aplus"><img class="community_icon" src="../img/plusicon.png" alt=""></a>
-        <div class="community_headline">
-            <h2>🔥HOT 게시글</h2>
-            <a href="{{route('lastboard.get')}}" class="cate_btn_go">전체보기</a>            
-        </div>
-	
-		
-        <div class="slider" id="slider">
-            @forelse ($data[0] as $item)
-            <a href="{{ route('board.show',['board'=>$item->board_id]) }}">
-                <div class="slide a-bordergo-hover">
-                    <div>
-                        <div class="hot-title">{{ $item->board_title }}</div>
-                        <div class="hot-content">{!! Str::limit($item->board_content, 130, '...') !!}</div>
-                    </div>
-                    <div class="community_bottom">
-                        <div class="hot-info">
-                            조회수 {{ $item->board_hits }}
-                        </div>
-                        <div class="hot-info">
-                            댓글 {{ $item->comments->count() }}
-                        </div>
-                    </div>
-                </div>
-            </a>
-            @empty
-                게시글이 없습니다.
-            @endforelse
-        </div>
-    </div>    
-        
-    <div class="favorite_slider-container"> 
-        <h2> 유행 중인 질병 </h2>
-        <div class="favorite_slider">
-            @forelse ($data[1] as $item)
-            <div class="favorite_slide">
-                <div class="favorite_slide-name">
-                    {{ $item->pandemic_name }}
-                </div> 
-                <div class="community_pandemic_content">
-                    {{ $item->pandemic_symptoms }}
-                </div>            
-            </div>
-            @empty
-                
-            @endforelse
-        </div>
-    </div>    
-    <div class="community_tag_hidden">
-        <div class="board-bc-gray">
-        <div class="community_tag_container"> 
-            <div class="community-favorite-show">
-                <h2>⭐관심 태그</h2>                    
-            </div>
-            <div style="margin: 5px 0 5px 20px;">
-                @forelse ($data[4] as $item)
-                <span>{{$item->hashtag_name}}</span>
-            @empty
-                등록된 관심태그가 없습니다
-            @endforelse
-            </div>
-            <div class="community_tag bordergo-hover" id="favoriteboard">
-                @forelse ($data[2] as $item)
-                <a href="{{ route('board.show',['board'=>$item->board_id]) }}">
-                    <span class="spantag-span-display-block">
-                        <span class="community-home-title">{{Str::limit($item->board_title, 30, '...')}}</span>
-                        <span class="community-home-content">{!! Str::limit($item->board_content, 100, '...') !!}</span>
-                    </span>
-                    <div class="community-fav-board-tag">
-                    @forelse ($item->board_tag as $value)
-                            <span>{{$value->hashtag_name}}</span>
-                            @empty
-                            
-                            @endforelse
-                    </div>
-                </a>
-                @if($loop->last)
-                <input type="hidden" name="favorite_num" value="{{ $item->board_id }}" id="favorite_num">
-                @endif
-                @empty
-            @endforelse
-            </div>
-            @if (count($data[2]) === 0)
-
-            @else
-            <div class="community_more_container">
-                <button class="community_more" onclick="favoriteBoard(); return false;" id="favoritebtn">더보기</button>
-                {{-- <p>{{count($data[2]) }}</p> --}}
-            </div>
-            @endif
-        </div>
+<main class="community-container">
+    <div class="community-big-div">
+        <a class="community-nav-bar" onclick="toggleDropdown()">카테고리</a>
+        <a class="community-nav-bar">최신글</a>
+        <a class="community-nav-bar">인기글</a>
+        <a class="community-nav-bar">관심태그</a>
     </div>
-    <div class="community_tag_container">        
-        <div class="community_headline">
-            <h2>최근 게시글</h2>
-        </div>
-        <div class="community_tag border-line-color-gray bordergo-hover" id="lastboardbox">
-            @forelse ($data[3] as $item)
-            <a href="{{ route('board.show',['board'=>$item->board_id]) }}">
-                <span class="spantag-span-display-block">
-                    <span class="community-home-title">{{Str::limit($item->board_title, 30, '...')}}</span>
-                    <span class="community-home-content">{!! Str::limit($item->board_content, 100, '...') !!}</span>
-                </span>
-            </a>
-            @if($loop->last)
-            <input type="hidden" name="last_num" value="{{ $item->board_id }}" id="last_num">
-            @endif
-
-            @empty
-                게시글이 없습니다.
-            @endforelse
-        </div>
-        @if (count($data[3]) === 0)
-            
-        @else
-        <div class="community_more_container" id="lastboardbtn">
-            <button class="community_more" onclick="lastBoard(); return false;">더보기</button>
-        </div>
-        @endif
+    <div class="dropdown-content" id="myDropdown">
+        <form method="get" id="category_id_form">
+            @csrf
+                <div class="board-mouse-cursor" onclick="showBoard(1); return false;">자유게시판</div>
+                <div class="board-mouse-cursor" onclick="showBoard(2); return false;">정보게시판</div>
+                <div class="board-mouse-cursor" onclick="showBoard(3); return false;">친목게시판</div>
+                <div class="board-mouse-cursor" onclick="showBoard(4); return false;">질문게시판</div>
+        </form>
     </div>
 </main>
 <script src="/js/community.js"></script>
--- team2/resources/views/layout/footer.blade.php
+++ team2/resources/views/layout/footer.blade.php
@@ -1,5 +1,25 @@
 <footer>
-	<div class="footer">
+	{{-- <div class="footer">
 		ⓒ 2023. team2 Co. all rights reserved.
-	</div>
+	</div> --}}
+	@if (!Str::contains(request()->url(), 'login') && !Str::contains(request()->url(), 'regist')
+	&& !Str::contains(request()->url(), 'mypage') && !Str::contains(request()->url(), 'categoryboard')
+	&& !Str::contains(request()->url(), 'boardcategory') && !Str::contains(request()->url(), 'board/') 
+	&& !Str::contains(request()->url(), 'hotboard') && !Str::contains(request()->url(), 'lastboard') && !Str::contains(request()->url(), 'favoriteboard')
+	&& !Str::contains(request()->url(), '/detail/'))
+		<div class="footer-container">
+			<div class="footer-icon-div">
+				<a href="#"><img class="footer-icon" src="/img/search.png" alt=""></a>
+			</div>
+			<div class="footer-icon-div">
+				<a href="#"><img class="footer-icon" src="/img/community.png" alt=""></a>
+			</div>
+			<div class="footer-icon-div">
+				<a href="#"><img class="footer-icon" src="/img/home.png" alt=""></a>
+			</div>
+			<div class="footer-icon-div">
+				<a href="#"><img class="footer-icon" src="/img/mypage.png" alt=""></a>
+			</div>
+		</div>
+	@endif
 </footer>
