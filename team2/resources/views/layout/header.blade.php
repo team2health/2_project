934c0c9d07dc1581ee8a637f6d4deff110216f7d
kanghwiya <fiminmy@gmail.com>
2024-01-04 16:41:19 +0900

    240104_임시 헤더 업로드

--- team2/resources/views/layout/header.blade.php
+++ team2/resources/views/layout/header.blade.php
@@ -1,21 +1,20 @@
 <header>
 	@if(request()->path() == 'board')
-	@if (request()->path() === 'board')
-		<div class="header-community-container">
-			<a class="cate_btn header-community-nav" onclick="toggleDropdown()">카테고리</a>
-			<div class="dropdown-content" id="myDropdown">
-				<form method="get" id="category_id_form">
-					@csrf
-						<div class="board-mouse-cursor" onclick="showBoard(1); return false;">자유게시판</div>
-						<div class="board-mouse-cursor" onclick="showBoard(2); return false;">정보게시판</div>
-						<div class="board-mouse-cursor" onclick="showBoard(3); return false;">친목게시판</div>
-						<div class="board-mouse-cursor" onclick="showBoard(4); return false;">질문게시판</div>
-				</form>
-			</div>
-			<a href="{{route('lastboard.get')}}" class="header-community-nav">최신글</a>
-			<a href="{{route('hotboard.get')}}" class="header-community-nav">인기글</a>
-			<a href="{{route('favoriteboard.get')}}" class="header-community-nav">관심태그</a>
+	<div class="header-community-container">
+		<a class="cate_btn header-community-nav" onclick="toggleDropdown()">카테고리</a>
+		<div class="dropdown-content" id="myDropdown">
+			<form method="get" id="category_id_form">
+				@csrf
+					<div class="board-mouse-cursor" onclick="showBoard(1); return false;">자유게시판</div>
+					<div class="board-mouse-cursor" onclick="showBoard(2); return false;">정보게시판</div>
+					<div class="board-mouse-cursor" onclick="showBoard(3); return false;">친목게시판</div>
+					<div class="board-mouse-cursor" onclick="showBoard(4); return false;">질문게시판</div>
+			</form>
 		</div>
+		<a href="" class="header-community-nav">최신글</a>
+		<a href="" class="header-community-nav">인기글</a>
+		<a href="" class="header-community-nav">관심태그</a>
+	</div>
 	@endif
 
 	@if (Str::contains(request()->url(), 'login') || Str::contains(request()->url(), 'regist')
@@ -27,6 +26,5 @@
 		<div class="header-mobile-backBtn"> < </div>
 		<div class="header-pageName" id="headerPageName">페이지이름</div>
 	</div>
-	@endif
 	@endif
 </header>
