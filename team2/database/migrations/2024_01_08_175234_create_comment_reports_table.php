0b890610825b2cb936c8b3d30f1615501f18f2a2
mk1206 <alsrod1400@naver.com>
2024-01-12 17:51:30 +0900

    20240112_admin 대시보드 통계

--- team2/database/migrations/2024_01_08_175234_create_comment_reports_table.php
+++ team2/database/migrations/2024_01_08_175234_create_comment_reports_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->increments('comment_report_id');
             $table->integer('comment_id');
             $table->integer('u_id');
+            $table->char('comment_reason_flg', 2);
+            $table->char('comment_report_complete', 1)->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
--- team2/public/js/admincommon.js
+++ team2/public/js/admincommon.js
@@ -1,14 +1,60 @@
 let sidebarItemHover = document.querySelectorAll('.sidebar-item');
 let currentPath = window.location.pathname + window.location.search;
 if(currentPath === '/admin/main') {
-    console.log('작동해');
-    console.log(sidebarItemHover[0]);
     sidebarItemHover[0].style. setProperty('background-color', '#b8d8ff', 'important');
     // sidebarItemHover[0].style.backgroundColor = '#b8d8ff';
     // sidebarItemHover[0].style.color = 'rgb(53, 44, 57)';
 }
 
-var myChart = echarts.init(document.getElementById('chart'));
+let myChart = echarts.init(document.getElementById('chart'));
+let MAINDATA = JSON.parse(document.getElementById('statistics').getAttribute('data-results'));
+let GENDERMAIN = document.getElementById('main-gender');
+
+let data = MAINDATA[0];
+let genderdata = MAINDATA[1];
+let boarddata = MAINDATA[2];
+let commentdata = MAINDATA[3];
+
+genderdata.forEach(element => {
+  let SPAN = document.createElement('div');
+  if(element.user_gender === '1') {
+    SPAN.innerHTML = '남성:'+element.gender_count;
+  } else if(element.user_gender === '2') {
+    SPAN.innerHTML = '여성:'+element.gender_count;
+  }
+  SPAN.classList = 'genderList';
+  GENDERMAIN.appendChild(SPAN);
+});
+
+let week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+let boardcnt = [];
+let bcount = 0;
+
+week.forEach(week => {
+  bcount = 0;
+
+  boarddata.forEach(element => {
+    if(element.week === week) {
+      bcount = element.cnt;
+    }
+  });
+  boardcnt.push(bcount);
+});
+
+let commentcnt = [];
+let ccount = 0;
+
+week.forEach(week => {
+  ccount = 0;
+
+  commentdata.forEach(element => {
+    if(element.week === week) {
+      ccount = element.cnt;
+    }
+  });
+  boardcnt.push(ccount);
+});
+
 
 option = {
   tooltip: {
@@ -20,7 +66,7 @@ option = {
   },
   series: [
     {
-      name: 'Access From',
+      name: '연령대',
       type: 'pie',
       radius: ['40%', '70%'],
       avoidLabelOverlap: false,
@@ -43,15 +89,56 @@ option = {
       labelLine: {
         show: false
       },
-      data: [
-        { value: 1048, name: 'Search Engine' },
-        { value: 735, name: 'Direct' },
-        { value: 580, name: 'Email' },
-        { value: 484, name: 'Union Ads' },
-        { value: 300, name: 'Video Ads' }
-      ]
+      data
     }
   ]
 };
 
 myChart.setOption(option);
+
+
+let myChart1 = echarts.init(document.getElementById('chart1'));
+
+option1 = {
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: ['board', 'comment']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      name: 'board',
+      type: 'line',
+      stack: 'Total',
+      data: boardcnt
+    },
+    {
+      name: 'comment',
+      type: 'line',
+      stack: 'Total',
+      data: [2, 3, 4, 7, 9, 2, 3]
+    }
+  ]
+};
+
+myChart1.setOption(option1);
