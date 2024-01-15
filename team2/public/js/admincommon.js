3798e96ecae17398b061e7fc3d65fc2b035ec476
mk1206 <alsrod1400@naver.com>
2024-01-16 00:41:02 +0900

    240116_admin regist modal

--- team2/public/js/admincommon.js
+++ team2/public/js/admincommon.js
@@ -5,3 +5,13 @@ if(currentPath === '/admin/main') {
     // sidebarItemHover[0].style.backgroundColor = '#b8d8ff';
     // sidebarItemHover[0].style.color = 'rgb(53, 44, 57)';
 }
+
+let ADMINMODAL = document.getElementById('admin_modal');
+
+function adminmodalon() {
+    ADMINMODAL.style.display = 'block';
+}
+
+function adminmodaloff() {
+    ADMINMODAL.style.display = 'none';
+}
--- team2/resources/views/adminpage/index.blade.php
+++ team2/resources/views/adminpage/index.blade.php
@@ -24,24 +24,30 @@
 				</div>
 			</div>
 			<br><br>
-			<span>2024년</span>
+			<div class="pandemic_box">2024년 부위-증상 통계</div>
+			<br><br>
 			<div class="card-header">
 				<div>
 					<div class="admin-index-ps">
 						<div class="admin-index-ps-mini">
 							<span>순위</span>
 							<span>부위 - 증상</span>
+							<span>2024년</span>
+							<span>2023년</span>
 						</div>
-						<span>조회수</span>
+						<span>생성일자</span>
 					</div>
 					<div></div>
 				</div>
 			</div>
-			<br>
-            <div class="button-box">
-                <input type="text" id="pandemic_name_insert" name="pandemic_name" class="insert-pandemic-input1" placeholder="질병명">
-                <input type="text" id="pandemic_symptom_insert" name="pandemic_symptom" class="insert-pandemic-input2" placeholder="증상">
-                <button type="button" class="admin-custom-btn custom-common-delete-btn" onclick="insertpandemic(); return false;">+</button>
+			<br><br><br>
+            <div class="button-box1">
+				<div class="pandemic_box">유행하는 질병</div>
+				<div class="pandemic_insert_box">
+					<input type="text" id="pandemic_name_insert" name="pandemic_name" class="insert-pandemic-input1" placeholder="질병명">
+					<input type="text" id="pandemic_symptom_insert" name="pandemic_symptom" class="insert-pandemic-input2" placeholder="증상">
+					<button type="button" class="admin-custom-btn custom-common-delete-btn" onclick="insertpandemic(); return false;">+</button>
+				</div>
             </div>
 			<br>
 			<div class="card-header pandemic-border-bottom">
