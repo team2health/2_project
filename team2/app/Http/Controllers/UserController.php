426c13de37029fc08944035d77cbc32243c1fc89
kanghwiya <fiminmy@gmail.com>
2024-01-17 18:29:02 +0900

    240117

--- team2/app/Http/Controllers/UserController.php
+++ team2/app/Http/Controllers/UserController.php
@@ -32,6 +32,9 @@ class UserController extends Controller
     
     public function registpost(Request $request) {
         Session::flush();
+        if(!session('email')){
+            return redirect()->route('regist.get');
+        }
         if(!isset($request->agreement_flg)) {
             return redirect()->route('regist.get')->with('agreement_Error', '1');
         }
